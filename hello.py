cb8e38fd7493bb7048310641bd92430693ccc754
Pavan Patil <pp8980929@gmail.com>
2025-02-10 10:14:20 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -6,6 +6,5 @@ def factorial(n):
     else:
         return n * factorial(n - 1)
 
-# Example usage
 number = int(input("Enter a number: "))
 print(f"The factorial of {number} is {factorial(number)}")
